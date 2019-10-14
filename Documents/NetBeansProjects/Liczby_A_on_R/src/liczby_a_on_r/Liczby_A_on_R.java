8b02ccff95dfd73eb908af2e2c662889b1d09a5c
stanad <mariusz1998@wp.pl>
2019-10-14 14:02:02 +0200

    Initial commit.

---
+++ Documents/NetBeansProjects/Liczby_A_on_R/src/liczby_a_on_r/Liczby_A_on_R.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package liczby_a_on_r;
+
+/**
+ *
+ * @author Student
+ */
+public class Liczby_A_on_R {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
